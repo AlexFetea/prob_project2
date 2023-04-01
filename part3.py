8c74025444d8e2f85556dde2485014417a2909e4
Ethan Ermovick <eeermo@gmail.com>
2023-04-01 15:40:38 -0400

    fixed rng

--- part3.py
+++ part3.py
@@ -1,13 +1,13 @@
-curr_random = 1000
+prev_random = 1000
 
 
 def get_next_random():
-    global curr_random
-    curr_random = (24693 * curr_random + 3517) % (2 ** 17)
-    return curr_random
+    global prev_random
+    prev_random = ((24693 * prev_random + 3517) % (2 ** 17))
+    return prev_random / (2**17)
 
 
 for i in range(1, 54):
-    get_next_random()
+    u = get_next_random()
     if i >= 51:
-        print(f"i: {i}, u_i: {curr_random}")
+        print(f"i: {i}, u_i: {u}")
